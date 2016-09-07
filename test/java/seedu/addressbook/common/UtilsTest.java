4620a0e91818e01a807234538b3c2df845cb70a3
cheo1994 <tehcy2@msn.com>
2016-09-07 14:58:55 +0800

    Test cases for isAnyNull completed. Started elementsAreUnique test.

---
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package seedu.addressbook.common;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+/**
+ * @author CheeYeo
+ *
+ */
+public class UtilsTest {
+
+	/**
+	 * Test method for {@link seedu.addressbook.common.Utils#isAnyNull(java.lang.Object[])}.
+	 */
+	@Test
+	public void isAnyNull_oneNullItem() {
+		Object nullObject = null;
+		boolean testResult = Utils.isAnyNull(nullObject);
+		assertTrue(testResult);
+	}
+	
+	@Test
+	public void isAnyNull_manyNullItems() {
+		Object nullObject1 = null;
+		Object nullObject2 = null;
+		Object nullObject3 = null;
+		boolean testResult = Utils.isAnyNull(nullObject1, nullObject2, nullObject3);
+		assertTrue(testResult);
+	}
+	
+	@Test
+	public void isAnyNull_oneNonNullItem() {
+		String nonNullObj = "non null string test";
+		boolean testResult = Utils.isAnyNull(nonNullObj);
+		assertFalse(testResult);
+	}
+	
+	@Test
+	public void isAnyNull_manyNonNullItem() {
+		int nonNullObj1 = 123;
+		String nonNullObj2 = "hello";
+		Object nonNullObj3 = new Object();
+		boolean testResult = Utils.isAnyNull(nonNullObj1, nonNullObj2, nonNullObj3);
+		assertFalse(testResult);
+	}
+
+	@Test
+	public void isAnyNull_noArgs() {
+		boolean testResult = Utils.isAnyNull();
+		assertFalse(testResult);
+	}
+	
+	/**
+	 * Test method for {@link seedu.addressbook.common.Utils#elementsAreUnique(java.util.Collection)}.
+	 */
+	@Test
+	public void elementsAreUnique_someDuplicates() {
+		ArrayList<Integer> listOfIntegers = new ArrayList<Integer>();
+		listOfIntegers.add(1);
+		listOfIntegers.add(2);
+		listOfIntegers.add(1);
+		assertFalse(Utils.elementsAreUnique(listOfIntegers));
+	}
+	
+	@Test
+	public void elementsAreUnique_uniqueElements() {
+		
+	}
+}
